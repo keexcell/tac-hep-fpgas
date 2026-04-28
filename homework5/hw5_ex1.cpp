be44eba964cefc8bd30e950a894d9e87749ad5ab
Kayleigh Excell <kexcell@wisc.edu>
2026-04-28 11:21:01 -0500

    Finishing HW 5

--- homework5/hw5_ex1.cpp
+++ homework5/hw5_ex1.cpp
@@ -3,29 +3,41 @@
 
 void read_data(int in_r[N], int out_r[N]) {
     for (int i = 0; i < N; i++) {
+//#pragma HLS pipeline II=2
         out_r[i] = in_r[i];  // Simple pass-through
     }
 }
 
 void compute_blur(int in_c[N], int out_c[N]) {
-    for (int i = 1; i < N - 1; i++) {
-        out_c[i] = (in_c[i - 1] + in_c[i] + in_c[i + 1]) / 3;  // Box blur operation
+    	for (int i = 1; i < N - 1; i++) {
+//#pragma HLS pipeline II=2
+     	out_c[i] = (in_c[i - 1] + in_c[i] + in_c[i + 1]) / 3;  // Box blur operation
     }
 }
 
 void write_data(int in_w[N], int out_w[N]) {
     for (int i = 0; i < N; i++) {
-        out_w[i] = in_w[i];  // Simple pass-through
+//#pragma HLS pipeline II=2
+  	out_w[i] = in_w[i];  // Simple pass-through
     }
 }
 
 void hw5_ex1(int A[N], int B[N]) {
     int temp1[N], temp2[N];
-
-    for (size_t i = 0; i < N; i++) 
-      temp2[i]=0;
-
+#pragma HLS array_partition variable=A complete
+#pragma HLS array_partition variable=B complete
+#pragma HLS array_partition variable=temp1 complete
+#pragma HLS array_partition variable=temp2 complete
+    
+    for (size_t i = 0; i < N; i++){
+//#pragma HLS pipeline II=2    
+//#pragma HLS latency min=4 max=8
+   	temp2[i]=0;
+    }
+  #pragma HLS pipeline II=2    
+  
     read_data(A, temp1);
     compute_blur(temp1, temp2);
     write_data(temp2, B);
+    
 }
